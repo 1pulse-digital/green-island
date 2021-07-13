87f74e5d9456acc280591de7edad06ede2fce8ac
Parson Gapa <parson@1pulse.co.za>
2021-07-13 14:15:43 +0200

    Configured TailwindCSS

---
+++ pages/about.tsx
@@ -0,0 +1,9 @@
+const about = () => {
+  return (
+    <div className={"mt-4 bg-gray-50 hover:bg-blue-400 md:hover:bg-green-200"}>
+      Ok this is gonna work
+    </div>
+  );
+};
+
+export default about;
