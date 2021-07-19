d9b4081d22f4481323a02ffc0bedb3384b270547
Daniella Neto <daniella@1pulse.co.za>
2021-07-19 08:18:53 +0200

    Add shop page and add Google Karla font

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import home from "../pages/home";
 import Image from "next/image";
 import hero from "../images/banner.png";
 
@@ -8,14 +7,14 @@ const Hero = () => {
     <div className={"w-full flex-wrap relative"}>
       <div
         className={
-          "font-sans md:font-serif  flex flex-col absolute top-0 left-0 justify-center items-left text-5xl font-bold z-10 text-gray-500 py-72 pl-20 w-1/2  "
+          "font-karla flex flex-col absolute top-0 left-0 justify-center items-left text-5xl font-bold z-10 text-gray-500 py-72 pl-20 w-1/2  "
         }
       >
         Perfect health is within your grasp.
       </div>
       <div
         className={
-          "font-sans  flex flex-col absolute top-0 left-0 justify-center text-2xl text-gray-500 items-left z-10 py-96 pl-20 mr-96 w-1/2  "
+          "font-karla flex flex-col absolute top-0 left-0 justify-center text-2xl text-gray-500 items-left z-10 py-96 pl-20 mr-96 w-1/2  "
         }
       >
         We are devoted to give our patients not only the tools they need to
---
+++ pages/shop.tsx
@@ -0,0 +1,18 @@
+import { Footer } from "../components/footer";
+import { Navbar } from "../components/navbar";
+import MainLayout from "../layouts/MainLayout";
+
+const Shop = () => {
+  return (
+    <MainLayout>
+      <div className="h-full bg-yellow-200">
+        Some normal ish stuff goes here
+        <div>more struff</div>
+        <div className="bg-red-400">taiwind stying works here</div>
+      </div>
+    </MainLayout>
+  );
+};
+
+export default Shop;
+
