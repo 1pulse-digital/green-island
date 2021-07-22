5f67bdad60dba62446835bb0996143de385da492
Daniella Neto <daniella@1pulse.co.za>
2021-07-22 22:57:24 +0200

    Update about section on home page

--- components/about.tsx
+++ components/about.tsx
@@ -6,39 +6,42 @@ export const About = () => {
   return (
     <div
       className={
-        "py-11 bg-gray-100 relative pt-6 px-4 sm:px-6 lg:px-8 flex-wrap  pb-14 flex-row"
+        " bg-gray-100 pt-6 px-4 sm:px-6 lg:px-8 grid grid-cols-2 p-0"
       }
     >
-      <div className={"w-1/2  pt-16 pl-20"}>
-        <Image src={bottle} alt="logo" />
+      <div className={"h-[700px] grid content-center"}>
+      <div className={"h-[500px] relative"}>
+        <Image
+          src={bottle}
+          alt="logo"
+          layout={"fill"}
+          objectFit={"contain"}
+        />
+      </div>
+      </div>
 
-        <div className={" w-1/2 h-full flex absolute top-0 left-0"}></div>
-
-        <div
+      <div className={"grid content-center px-12"}>
+        <h1
           className={
-            " font-sans md:font-serif h-full  absolute top-0 left-0  text-5xl  z-10 text-gray-500  text-center pt-48  font-bold  flex-auto  ml-96 w-1/2 "
+            "font-sans md:font-serif h-full text-5xl  text-gray-500   font-bold  "
           }
         >
           Your body is incredible
-        </div>
-        <div
-          className={
-            " font-sans md:font-serif h-full  absolute top-0 left-0    z-10 text-gray-500  text-center pt-72 pl-48  font-bold  flex-auto  ml-96 w-1/2  "
-          }
-        >
+        </h1>
+        <p>
           It’s capable of extraordinary repair and self-healing. But to access
           that power ​you must remove the obstacles slowing it down and provide
           the tools it needs for the job. That’s precisely what we HELP YOU TO
           do!
-        </div>
-        {/* <div
+        </p>
+      </div>
+      {/* <div
           className={
             "bg-transparent hover:bg-blue text-blue-dark font-semibold uppercase hover:text-white py-2 px-8 border border-black hover:border-blue rounded    flex-auto w-1/2  justify-center pl-96"
           }
         >
           <button>Shop All</button>
         </div> */}
-      </div>
     </div>
   );
 };
