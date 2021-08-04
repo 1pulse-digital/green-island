76a38fa3dcd769fd0f66cef0279d720987a9788b
Erick van der Linde <erick@andile.io>
2021-08-04 23:25:28 +0200

    Update the shop page

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -1,13 +1,43 @@
 import MainLayout from "../layouts/MainLayout";
 import { ShopBanner } from "../components/shopBanner";
 import { Suppliers } from "../components/suppliers";
+import { Product } from "../types/product";
+import useSWR from "swr";
+import { fetchAPI } from "../lib/api";
 
-const Shop = () => {
+export interface ShopProps {
+}
+
+const useProducts = () => {
+  const { data, error } = useSWR("/products", fetchAPI);
+  return {
+    products: data,
+    isLoading: !error && !data,
+    error,
+  };
+};
+
+const Shop = (props: ShopProps) => {
+  const { products, isLoading, error } = useProducts();
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
+  if (error) {
+    return <div>Error: {JSON.stringify(error)}</div>;
+  }
+  console.log("The products:", products);
   return (
     <MainLayout>
-      <ShopBanner/>
-      The shop goes here
-      <Suppliers/>
+      <ShopBanner />
+
+      There are some products
+      {products.map((p: Product) => {
+        return (
+          <div key={p.id}>{p.name}</div>
+        );
+      })}
+      <Suppliers />
     </MainLayout>
   );
 };
