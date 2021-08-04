c69697819c11da1c5908adb7346b24eaa06a8b39
Erick van der Linde <erick@andile.io>
2021-08-04 18:38:50 +0200

    Fixed the spacing

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -1,8 +1,6 @@
-import {Footer} from "../components/footer";
-import {Navbar} from "../components/navbar";
 import MainLayout from "../layouts/MainLayout";
-import {ShopBanner} from "../components/shopBanner";
-import {Suppliers} from "../components/suppliers";
+import { ShopBanner } from "../components/shopBanner";
+import { Suppliers } from "../components/suppliers";
 
 const Shop = () => {
   return (
