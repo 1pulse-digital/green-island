815e743c264799671dd1ba336003505061e8174a
Erick van der Linde <erick@andile.io>
2021-08-04 18:25:38 +0200

    Just add dummy content

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -1,14 +1,15 @@
-import { Footer } from "../components/footer";
-import { Navbar } from "../components/navbar";
+import {Footer} from "../components/footer";
+import {Navbar} from "../components/navbar";
 import MainLayout from "../layouts/MainLayout";
-import { ShopBanner } from "../components/shopBanner";
-import { Suppliers } from "../components/suppliers";
+import {ShopBanner} from "../components/shopBanner";
+import {Suppliers} from "../components/suppliers";
 
 const Shop = () => {
   return (
     <MainLayout>
-      <ShopBanner />
-      <Suppliers />
+      <ShopBanner/>
+      The shop goes here
+      <Suppliers/>
     </MainLayout>
   );
 };
