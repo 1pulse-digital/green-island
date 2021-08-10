85825a2fe953a5c5c3cccb7b0fd608347b35ffea
Daniella Neto <daniella@1pulse.co.za>
2021-08-11 00:00:10 +0200

    Fix the Product type

--- types/product.d.ts
+++ types/product.d.ts
@@ -1,14 +1,17 @@
+import { Picture } from "./picture";
+
 export interface Product {
   id: number;
   name: string;
   description: string;
   meta_description: string;
   meta_title: string;
-  price: DeviceMotionEventAcceleration;
+  price: number;
   slug: string;
   content: string;
   published_at: Date;
   created_at: Date;
   updated_at: Date;
   variation: boolean;
+  image: Picture;
 }
