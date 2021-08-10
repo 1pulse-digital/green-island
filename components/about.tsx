7b1a9170f8bc4f1dadc33cbbf7af780d4e161d40
Daniella Neto <daniella@1pulse.co.za>
2021-08-11 00:01:13 +0200

    Update styling

--- components/about.tsx
+++ components/about.tsx
@@ -9,26 +9,33 @@ export const About = () => {
         <div className={"grid sm:grid-cols-1 md:grid-cols-2 h-[700px]  "}>
           {/* Left column */}
           <div className={"  grid content-center justify-center"}>
-            <div className={"  top-20 bottom-32 right-0   "}>
+            <div >
               <Image src={bottle} alt="homeropath icon" />
             </div>
           </div>
           <div className={" content-center  px-10  grid"}>
             <h1
               className={
-                "md:text-5xl pb-4 font-bold  grid sm: text-3xl text-primary"
+                "md:text-5xl pb-4 font-medium  grid sm: text-3xl text-primary"
               }
             >
               Your body is incredible.
             </h1>
             <p
               className={
-                "mr-32 content-center  grid sm:max-w-sm md:max-w-md text-gray-700 "
+                "mr-32 content-center  grid sm:max-w-sm md:max-w-md text-gray-700 font-light"
               }
             >
               It is capable of healing itself. To access this power ​you must
               remove the obstacles in its way, provide the tools it needs for
-              the job. That’s what we help you to do!
+              the job.
+            </p>
+            <p
+              className={
+                "mr-32 content-center  grid sm:max-w-sm md:max-w-md text-gray-700 font-light pt-4"
+              }
+            >
+              That’s what we help you to do!
             </p>
             <div className="flex items-center">
               <button
