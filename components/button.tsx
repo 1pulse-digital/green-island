66824494034e642325d6f17d42941bbd3881e4df
Erick van der Linde <erick@andile.io>
2021-08-13 11:05:16 +0200

    Add onClick to custom Button

--- components/button.tsx
+++ components/button.tsx
@@ -1,22 +1,24 @@
-import cn from "classnames"
+import cn from "classnames";
+import { ReactNode, MouseEvent } from "react";
 
 export interface ButtonProps {
-  children: React.ReactNode;
+  children: ReactNode;
   className?: string;
-  color?: "primary" | "secondary"
+  color?: "primary" | "secondary";
+  onClick?: (event: MouseEvent<HTMLButtonElement>) => void
 }
 
 export const Button = (props: ButtonProps) => {
-  let colorClassNames = "bg-primary text-white "
+  let colorClassNames = "bg-primary text-white ";
   if (props.color === "secondary") {
-    colorClassNames = "bg-secondary text-primary"
+    colorClassNames = "bg-secondary text-primary";
   }
-  const combinedClassName = cn(colorClassNames, "rounded-full py-2 hover:bg-secondary px-4 md:px-10 py-3 shadow", props.className)
+  const combinedClassName = cn(colorClassNames, "rounded-full py-2 hover:bg-secondary px-4 md:px-10 py-3 shadow", props.className);
   return (
-    <button className={combinedClassName}>
+    <button className={combinedClassName} onClick={props.onClick}>
       {props.children}
     </button>
   );
 };
 
-export default Button
+export default Button;
