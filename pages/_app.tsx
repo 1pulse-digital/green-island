c4a56af24c5ffff43c1353fe3aa4383cc2ef853e
Daniella Neto <daniella@1pulse.co.za>
2021-08-16 08:50:11 +0200

    fix build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Provider } from "next-auth/client";
+// import { Provider } from "next-auth/client";
 import CartContext from "../contexts/cartContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider session={pageProps.session}>
-      <CartContext>
-        <Component {...pageProps} />
-      </CartContext>
-    </Provider>
+    // <Provider session={pageProps.session}>
+    <CartContext>
+      <Component {...pageProps} />
+    </CartContext>
+    // </Provider>
   );
 }
 
