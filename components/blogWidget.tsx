c40ee25c400aee43172d07508de49b2cbd986fb4
Daniella Neto <daniella@1pulse.co.za>
2021-08-19 13:22:26 +0200

    Update blog page

--- components/blogWidget.tsx
+++ components/blogWidget.tsx
@@ -64,8 +64,8 @@ export const BlogWidget = (props: BlogWidgetProps) => {
               <Link href="/blogs">
                 <a>
                   <button
-                    className={"flex text-secondary hover:text-primary py-5"}>
-                    Read more{" "}
+                    className={"flex text-secondary hover:text-primary py-2"}>
+                    Read more
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       className={"h-6 w-6 self-center pl-2"}
