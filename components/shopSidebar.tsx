2b07f728af7797694fcc6d2b8b6ec72982e1971a
Erick van der Linde <erick@andile.io>
2021-08-29 12:38:22 +0200

    Build shop sidebar from backend data

--- components/shopSidebar.tsx
+++ components/shopSidebar.tsx
@@ -1,21 +1,47 @@
 import React from "react";
+import { useProductCategories } from "../lib/api";
+import { ProductCategory } from "../types/productCategory";
 
-const SingleItem = ({ title }: { title: string }) => {
+interface SingleItemProps {
+  title: string;
+  checked: boolean;
+  onChange: () => void;
+}
+
+const SingleItem = (props: SingleItemProps) => {
   return (
     <label className="flex items-center space-x-3 mb-3 ">
       <input
         type="checkbox"
-        name="checked-demo"
-        className=" bg-white bg-check h-6 w-6 border border-gray-300 checked:bg-blue-500 checked:border-transparent focus:outline-none"
+        name={`${props.title.split(" ").join("-")}`}
+        className="bg-white bg-check h-6 w-6 border border-gray-300 checked:bg-blue-500 checked:border-transparent focus:outline-none"
+        checked={props.checked}
+        onChange={props.onChange}
+        value=""
       />
       <span className="truncate text-gray-700 dark:text-white font-normal">
-        {title}
+        {props.title}
       </span>
     </label>
   );
 };
 
-export const ShopSidebar = () => {
+export interface ShopSidebarProps {
+  selectedCategories: number[];
+  onSelectCategory: (id: number) => void;
+}
+
+export const ShopSidebar = (props: ShopSidebarProps) => {
+  const { productCategories, isLoading, error } = useProductCategories();
+
+  const isChecked = (id: number): boolean => {
+    return props.selectedCategories.findIndex((i) => i === id) > 0;
+  };
+
+  if (isLoading) {
+    return <div>loading</div>;
+  }
+
   return (
     <div className={"bg-gray-100 h-full py-5 px-10 md:px-10 lg:px-20 "}>
       <div className="flex flex-col sm:justify-center">
@@ -25,7 +51,7 @@ export const ShopSidebar = () => {
             <input
               type="search"
               className="bg-white p-3 w-full"
-              placeholder="Search products..."></input>
+              placeholder="Search products..." />
           </div>
 
           {/* Shop by Category */}
@@ -34,11 +60,20 @@ export const ShopSidebar = () => {
               Shop by Category
             </p>
 
-            {/*  test */}
+            {isLoading && <div className={"animate-pulse"}>Loading</div>}
 
-            <div className={"font-karla"}>
-              <SingleItem title="Hormonal Health" />
-              <SingleItem title="Gut Health" />
+            {!isLoading && (
+              <div className={"font-karla"}>
+                {productCategories.map((item: ProductCategory) => (
+                  <SingleItem
+                    key={item.id}
+                    title={item.name}
+                    checked={isChecked(item.id)}
+                    onChange={() => props.onSelectCategory(item.id)}
+                  />
+                ))}
+
+                {/* <SingleItem title="Gut Health" />
               <SingleItem title="Skin Health" />
               <SingleItem title="Mental Health" />
               <SingleItem title="Weight and Metabolism" />
@@ -48,26 +83,25 @@ export const ShopSidebar = () => {
               <SingleItem title="Stress Management" />
               <SingleItem title="Thyroid Support" />
               <SingleItem title="Infection Management" />
-              <SingleItem title="Pain Management" />
-            </div>
-
-            {/*  end */}
+              <SingleItem title="Pain Management" /> */}
+              </div>
+            )}
           </div>
 
-          {/* Shop by Prouct Type */}
+          {/* Shop by Product Type */}
           <div>
             <p className="text-primary font-bold w-full my-10 text-2xl font-karla">
               Shop by product type
             </p>
             <div>
-              <SingleItem title="Multivitamins" />
-              <SingleItem title=" Individual Nutrients" />
-              <SingleItem title="Digestive Aids" />
-              <SingleItem title=" Probiotics" />
-              <SingleItem title=" Prebiotics" />
-              <SingleItem title="Essential Fatty Acids" />
-              <SingleItem title="Immune Enhancers" />
-              <SingleItem title="Kid's health" />
+              {/*<SingleItem title="Multivitamins" />*/}
+              {/*<SingleItem title=" Individual Nutrients" />*/}
+              {/*<SingleItem title="Digestive Aids" />*/}
+              {/*<SingleItem title=" Probiotics" />*/}
+              {/*<SingleItem title=" Prbiotics" />*/}
+              {/*<SingleItem title="Essential Fatty Acids" />*/}
+              {/*<SingleItem title="Immune Enhancers" />*/}
+              {/*<SingleItem title="Kid's health" />*/}
             </div>
           </div>
 
@@ -79,13 +113,13 @@ export const ShopSidebar = () => {
             </p>
             {/* test1 */}
             <div>
-              <SingleItem title=" Capsule" />
-              <SingleItem title=" Softgel" />
-              <SingleItem title=" Tablet" />
-              <SingleItem title="  Powder" />
-              <SingleItem title="Shake" />
-              <SingleItem title=" Liquid" />
-              <SingleItem title="Granules" />
+              {/*<SingleItem title=" Capsule" />*/}
+              {/*<SingleItem title=" Softgel" />*/}
+              {/*<SingleItem title=" Tablet" />*/}
+              {/*<SingleItem title="  Powder" />*/}
+              {/*<SingleItem title="Shake" />*/}
+              {/*<SingleItem title=" Liquid" />*/}
+              {/*<SingleItem title="Granules" />*/}
             </div>
           </div>
         </nav>
--- pages/shop.tsx
+++ pages/shop.tsx
@@ -2,22 +2,36 @@ import MainLayout from "../layouts/MainLayout";
 import { ShopBanner } from "../components/shopBanner";
 import { Suppliers } from "../components/suppliers";
 import { Product } from "../types/product";
-import useSWR from "swr";
-import { fetchAPI, useProducts } from "../lib/api";
+import { useProducts } from "../lib/api";
 import ProductWidget from "../components/productWidget";
 import { ShopSidebar } from "../components/shopSidebar";
-import { useEffect } from "react";
+import { useState } from "react";
 
 export interface ShopProps {
 }
 
+interface ProductFilter {
+  categories: number[];
+}
+
 const Shop = (props: ShopProps) => {
   const { products, isLoading, error } = useProducts();
+  const [filter, setFilter] = useState<ProductFilter>({
+    categories: [],
+  });
 
-  if (isLoading) {
-    // TODO: This loader must be in the frame
-    return <div>Loading</div>;
-  }
+  const onSelectCategory = (id: number) => {
+    const idx = filter.categories.findIndex((i) => i === id);
+    if (idx === -1) {
+      const newCategories = [...filter.categories, id];
+      setFilter({ ...filter, categories: newCategories });
+    } else {
+      const newCategories = [...filter.categories];
+      newCategories.splice(idx);
+
+      setFilter({ ...filter, categories: newCategories });
+    }
+  };
 
   if (error) {
     // TODO: We need to handle this error better
@@ -27,17 +41,25 @@ const Shop = (props: ShopProps) => {
   return (
     <MainLayout>
       <ShopBanner />
-      <div className={"grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 justify-center"}>
-        <ShopSidebar />
+      <div
+        className={
+          "grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 justify-center"
+        }>
+        <ShopSidebar
+          onSelectCategory={onSelectCategory}
+          selectedCategories={filter.categories}
+        />
 
         <div className="p-5 md:p-7 col-span-2 xl:col-span-3">
           <p className={"font-karla pb-10"}>Home Digestive</p>
-          <div className="flex flex-wrap gap-4">
-            {products.map((p: Product) => {
-              return <ProductWidget key={p.id} product={p} />;
-            })}
-            {/*  TODO: The loader should probably go here somewhere */}
-          </div>
+          {!isLoading && (
+            <div className="flex flex-wrap gap-4">
+              {products.map((p: Product) => {
+                return <ProductWidget key={p.id} product={p} />;
+              })}
+            </div>
+          )}
+          {!isLoading && <div className={"animate-pulse"}>Loading</div>}
         </div>
       </div>
 
