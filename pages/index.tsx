69a6471b462d81a85fa47fe94c9b9e848acee7ba
Erick van der Linde <erick@andile.io>
2021-09-06 20:25:30 +0200

    Remove commented out code

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,57 +42,8 @@ const Home = (props: HomeProps) => {
       <FeaturedProducts products={featuredProducts} />
 
       {/* Testimonials (What our clients have to say) */}
-      {/* TODO: Use a react carousel component here: */}
       <div className={"bg-primary text-center h-[500px] grid content-center"}>
         <TestimonialCarousel />
-
-        {/* <div className={"grid grid-cols-3 h-20 items-center"}>
-          <div className={""}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className={
-                "h-7 w-7 bg-white rounded-full float-right	 fill-current text-primary"
-              }
-              viewBox="0 0 20 20"
-              fill="currentColor">
-              <path
-                fillRule="evenodd"
-                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-          <div className={"flex justify-center"}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className={"h-10 w-10  fill-current text-white"}
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-              />
-            </svg>
-          </div>
-          <div className={""}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className={
-                "h-7 w-7 bg-white rounded-full	 fill-current text-primary"
-              }
-              viewBox="0 0 20 20"
-              fill="currentColor">
-              <path
-                fillRule="evenodd"
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-        </div> */}
       </div>
 
       {/* Blogs */}
