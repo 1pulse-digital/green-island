4d8f8fe0a15fb7ff043fd1b8d8c3e501e6becd0c
Erick van der Linde <erick@andile.io>
2021-09-09 00:03:41 +0200

    Cleanup

--- components/checkout/shippingMethod.tsx
+++ components/checkout/shippingMethod.tsx
@@ -1,7 +1,6 @@
 import { RadioGroup } from "@headlessui/react";
 import { useState } from "react";
 import { prettyPrice } from "../../lib/calc";
-import cn from "classnames";
 
 export interface ShippingMethodProps {
   shipping: boolean;
@@ -20,34 +19,31 @@ export const ShippingMethod = (props: ShippingMethodProps) => {
         </RadioGroup.Label>
         <div className={"grid gap-2 mt-2"}>
           <RadioGroup.Option value="courier">
-            {({ checked }) => (
-              <>
-                <input
-                  type="checkbox"
-                  name={"courier"}
-                  className="w-6 h-6 bg-white rounded border-gray-300 hover:cursor-pointer bg-check text-primary focus:ring-secondary"
-                  checked={props.shipping}
-                  onChange={() => props.setShipping(!props.shipping)}
-                  value=""
-                />
-                <span className={"ml-2 text-gray-700 whitespace-nowrap"}>Courier +( {prettyPrice(props.shippingPrice)} )</span>
-              </>
-            )}
+            <>
+              <input
+                type="checkbox"
+                name={"courier"}
+                className="w-6 h-6 bg-white rounded border-gray-300 hover:cursor-pointer bg-check text-primary focus:ring-secondary"
+                checked={props.shipping}
+                onChange={() => props.setShipping(!props.shipping)}
+                value=""
+              />
+              <span
+                className={"ml-2 text-gray-700 whitespace-nowrap"}>Courier +( {prettyPrice(props.shippingPrice)} )</span>
+            </>
           </RadioGroup.Option>
           <RadioGroup.Option value="pickup">
-            {({ checked }) => (
-              <>
-                <input
-                  type="checkbox"
-                  name={"pickup"}
-                  className="w-6 h-6 bg-white rounded border-gray-300 hover:cursor-pointer bg-check text-primary focus:ring-secondary"
-                  checked={!props.shipping}
-                  onChange={() => props.setShipping(!props.shipping)}
-                  value=""
-                />
-                <span className={cn("ml-2 text-gray-700")}>Pick up in store (Free)</span>
-              </>
-            )}
+            <>
+              <input
+                type="checkbox"
+                name={"pickup"}
+                className="w-6 h-6 bg-white rounded border-gray-300 hover:cursor-pointer bg-check text-primary focus:ring-secondary"
+                checked={!props.shipping}
+                onChange={() => props.setShipping(!props.shipping)}
+                value=""
+              />
+              <span className={"ml-2 text-gray-700"}>Pick up in store (Free)</span>
+            </>
           </RadioGroup.Option>
         </div>
       </RadioGroup>
