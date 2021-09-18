9738fbb47c6411983627836c8c6f4b6e30f4fc2a
Parson Gapa <parson@1pulse.co.za>
2021-09-18 14:47:24 +0200

    Update testimonial component

--- components/home/testimonial.tsx
+++ components/home/testimonial.tsx
@@ -32,7 +32,7 @@ export const TestimonialCarousel = () => {
     <div className="  grid grid-cols-1 py-10 sm:h-  md:grid-cols-1 md:px-32 lg:grid-cols-1 xl:grid-cols-1  ">
       <h1
         className={
-          "     text-5xl md:text-5xl z-10 text-white font-medium font-karla pb-12 "
+          " sm:px-10 text-4xl   md:text-4xl  z-10 text-white font-medium font-karla pb-12 "
         }>
         What our clients have to say
       </h1>
@@ -46,7 +46,10 @@ export const TestimonialCarousel = () => {
         // renderArrowPrev={customPrevArrow}
       >
         <div className={"grid mb-20 "}>
-          <p className={"italic text-2xl text-white font-light  "}>
+          <p
+            className={
+              "italic md:text-2xl sm:text-xl  text-white font-light  "
+            }>
             “Dr Robin is passionate about healing people, finding the cause of
             the problem and then addressing it.<br></br> He explains everything
             so well really feel that at last there is hope.”
@@ -54,7 +57,7 @@ export const TestimonialCarousel = () => {
         </div>
 
         <div className={"grid"}>
-          <p className={"italic text-2xl text-white font-light "}>
+          <p className={"italic md:text-2xl sm:text-xl text-white font-light "}>
             “Dr Robin Is the most incredible doctor I have ever worked with! I
             cannot recommend anyone more highly!<br></br> Even my little girl of
             1 and a half was so impressed with him and sat and did everything he
@@ -64,7 +67,7 @@ export const TestimonialCarousel = () => {
         </div>
 
         <div className={"grid"}>
-          <p className={"italic text-2xl text-white font-light "}>
+          <p className={"italic md:text-2xl sm:text-xl text-white font-light "}>
             “Personally I appreciate such a thorough consultation that is not
             rushed! Dr Kohler looks for the root cause<br></br> (even root of
             the root issue ) and treats it naturally. Always such a positive
@@ -73,7 +76,7 @@ export const TestimonialCarousel = () => {
         </div>
 
         <div className={"grid"}>
-          <p className={"italic text-2xl text-white font-light "}>
+          <p className={"italic md:text-2xl sm:text-xl text-white font-light "}>
             “My experience with Dr. Robin Kohler can best be described as
             "transformative". <br></br>I feel like FINALLY someone has the
             intelligence and capability to treat the whole person. I highly
