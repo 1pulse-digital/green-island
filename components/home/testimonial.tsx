d02b1bcc94c59fb7203c14a8631314ac866585f6
Parson Gapa <parson@1pulse.co.za>
2021-09-20 12:42:31 +0200

    Testimonial Component Updates

--- components/home/testimonial.tsx
+++ components/home/testimonial.tsx
@@ -29,10 +29,10 @@ export const TestimonialCarousel = () => {
   );
 
   return (
-    <div className="  grid grid-cols-1 py-10 sm:h-  md:grid-cols-1 md:px-32 lg:grid-cols-1 xl:grid-cols-1  ">
+    <div className="  py-10  md:px-32   w-80 md:w-3/4  lg:w-full      ">
       <h1
         className={
-          " sm:px-10 text-4xl   md:text-4xl  z-10 text-white font-medium font-karla pb-12 "
+          "     md:text-4xl  z-10 text-white font-medium font-karla pb-12  "
         }>
         What our clients have to say
       </h1>
@@ -45,42 +45,63 @@ export const TestimonialCarousel = () => {
         // renderArrowNext={customNextArrow}
         // renderArrowPrev={customPrevArrow}
       >
-        <div className={"grid mb-20 "}>
+        <div className={"  lg:mb-32 md:mb-20 sm:mb-52   "}>
           <p
             className={
-              "italic md:text-2xl sm:text-xl  text-white font-light  "
+              "  italic lg:text-2xl md:text-2xl sm:text-1xl text-white font-light  "
             }>
             “Dr Robin is passionate about healing people, finding the cause of
-            the problem and then addressing it.<br></br> He explains everything
-            so well really feel that at last there is hope.”
+            the problem and then addressing it.
+            <p>
+              {" "}
+              He explains everything so well really feel that at last there is
+              hope.”
+            </p>
           </p>
         </div>
 
-        <div className={"grid"}>
-          <p className={"italic md:text-2xl sm:text-xl text-white font-light "}>
+        <div className={""}>
+          <p
+            className={
+              "italic lg:text-2xl md:text-2xl sm:text-1xl  text-white font-light "
+            }>
             “Dr Robin Is the most incredible doctor I have ever worked with! I
-            cannot recommend anyone more highly!<br></br> Even my little girl of
-            1 and a half was so impressed with him and sat and did everything he
-            needed her to do!<br></br> I never leave there not feeling positive
-            and helped!”
+            cannot recommend anyone more highly!
+            <p>
+              {" "}
+              Even my little girl of 1 and a half was so impressed with him and
+              sat and did everything he needed her to do!
+              <p> I never leave there not feeling positive and helped!”</p>
+            </p>
           </p>
         </div>
 
-        <div className={"grid"}>
-          <p className={"italic md:text-2xl sm:text-xl text-white font-light "}>
+        <div className={""}>
+          <p
+            className={
+              "italic lg:text-2xl md:text-2xl sm:text-1xl  text-white font-light "
+            }>
             “Personally I appreciate such a thorough consultation that is not
-            rushed! Dr Kohler looks for the root cause<br></br> (even root of
-            the root issue ) and treats it naturally. Always such a positive
-            experience.”
+            rushed! Dr Kohler looks for the root cause
+            <p>
+              {" "}
+              (even root of the root issue ) and treats it naturally. Always
+              such a positive experience.”
+            </p>
           </p>
         </div>
 
-        <div className={"grid"}>
-          <p className={"italic md:text-2xl sm:text-xl text-white font-light "}>
+        <div className={""}>
+          <p
+            className={
+              "italic lg:text-2xl md:text-2xl sm:text-1xl  text-white font-light "
+            }>
             “My experience with Dr. Robin Kohler can best be described as
-            "transformative". <br></br>I feel like FINALLY someone has the
-            intelligence and capability to treat the whole person. I highly
-            recommend his practice.”
+            "transformative".{" "}
+            <p>
+              I feel like FINALLY someone has the intelligence and capability to
+              treat the whole person. I highly recommend his practice.”
+            </p>
           </p>
         </div>
       </Carousel>
