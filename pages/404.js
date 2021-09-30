03f63b74a3eff7474513e4a4962e216f0fd0d131
Parson Gapa <parson@1pulse.co.za>
2021-09-30 10:47:30 +0200

    1PULSE-436-Link 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,8 @@
 import { Button } from "../components/button";
 export default function Custom404() {
   return (
-    <div className={"bg-gray-100 h-[700px]"}>
-      <div className={"   h-[100px] py-32   "}>
+    <div className={" "}>
+      <div className={"   h-[100px] py-48   "}>
         <p
           className={
             "grid-cols justify-items-center text-center py-10 text-9xl"
