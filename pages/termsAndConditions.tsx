4f389a24b4cc8c7c88f79403f79864221100836e
Parson Gapa <parson@1pulse.co.za>
2021-10-04 11:40:36 +0200

    PULSE-480 -Rename terms.tsx

---
+++ pages/termsAndConditions.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { TermsConditions } from "../components/termsConditions";
+import MainLayout from "../layouts/MainLayout";
+
+const Terms = () => {
+  return (
+    <MainLayout>
+      <div>
+        <TermsConditions />
+      </div>
+    </MainLayout>
+  );
+};
+export default Terms;
