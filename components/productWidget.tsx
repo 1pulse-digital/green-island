308a233b3aa2b6b424a462581e501a2f502d058d
Parson Gapa <parson@1pulse.co.za>
2021-10-04 11:57:44 +0200

    PULSE-314 -Add a Star Rating to Product Widget

--- components/productWidget.tsx
+++ components/productWidget.tsx
@@ -7,7 +7,7 @@ import placeholder from "../images/2.jpg";
 import { strapiLoader } from "../lib/media";
 import ReactTooltip from "react-tooltip";
 import { prettyPrice } from "../lib/calc";
-
+import ReactStars from "react-rating-stars-component";
 
 export interface ProductWidgetProps {
   product: Product;
@@ -56,14 +56,16 @@ const ProductWidget = (props: ProductWidgetProps) => {
             alt={"Placeholder image for product"}
           />
         )}
-
       </div>
 
       <div className={"h-32 grid grid-cols-3"}>
         <div className={" px-4 py-6 text-left col-span-2"}>
-          <span className={"text-primary text-xl leading-tight line-clamp-2"}>{product.name}</span>
+          <span className={"text-primary text-xl leading-tight line-clamp-2"}>
+            {product.name}
+          </span>
           <p className="text-green-500">In stock</p>
           <p className={"text-primary "}>{prettyPrice(product.price)}</p>
+          <ReactStars activeColor="blue" size={20} isHalf={true} />
         </div>
 
         <div
@@ -109,25 +111,30 @@ const ProductWidget = (props: ProductWidgetProps) => {
             className="self-center pl-2 w-5 h-5 sm:w-7 md:h-7"
             viewBox="0 0 20 20"
             fill="currentColor">
-            <path
-              d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
+            <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
           </svg>
-          {cartCount && (<span className={"ml-1"}>({cartCount})</span>)}
+          {cartCount && <span className={"ml-1"}>({cartCount})</span>}
         </Button>
       </div>
 
       {product.availability !== "otc" && (
-        <div className="absolute top-0 right-0 p-2 text-primary" data-tip="Prescription only product">
-          <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd"
-                  d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                  clipRule="evenodd" />
+        <div
+          className="absolute top-0 right-0 p-2 text-primary"
+          data-tip="Prescription only product">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-5 h-5"
+            viewBox="0 0 20 20"
+            fill="currentColor">
+            <path
+              fillRule="evenodd"
+              d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+              clipRule="evenodd"
+            />
           </svg>
         </div>
       )}
       <ReactTooltip place="left" type="dark" effect="solid" />
-
-
     </div>
   );
 };
--- components/singleProductWidget.tsx
+++ components/singleProductWidget.tsx
@@ -2,6 +2,10 @@ import { Product } from "../types/product";
 import Image from "next/image";
 import { strapiLoader } from "../lib/media";
 import { prettyPrice } from "../lib/calc";
+import ReactStars from "react-rating-stars-component";
+import React, { useState } from "react";
+import { StarIcon } from "@heroicons/react/solid";
+import App from "./app";
 
 export interface ProductWidget1Props {
   product: Product;
@@ -17,15 +21,16 @@ const ProductWidget1 = (props: ProductWidget1Props) => {
   return (
     <div className={"h-full w-full"}>
       <div
-        className={
-          "grid grid-cols-1 md:grid-cols-2 font-karla bg-gray-50  "
-        }>
+        className={"grid grid-cols-1 md:grid-cols-2 font-karla bg-gray-50  "}>
         <div className=" relative self-center h-[600px] w-[600px]">
           <Image
             layout="fill"
             objectFit="cover"
             loader={strapiLoader}
-            src={product.image?.formats.small.url || "https://image.shutterstock.com/image-vector/ui-image-placeholder-wireframes-apps-260nw-1037719204.jpg"}
+            src={
+              product.image?.formats.small.url ||
+              "https://image.shutterstock.com/image-vector/ui-image-placeholder-wireframes-apps-260nw-1037719204.jpg"
+            }
             alt={product.image?.alternativeText || "Product image"}
           />
         </div>
@@ -36,29 +41,28 @@ const ProductWidget1 = (props: ProductWidget1Props) => {
           </span>
           {/* Product Rating Starts */}
           <div className={"flex flex-auto pt-6"}>
+            <ReactStars activeColor="blue" size={30} isHalf={true} />
+
             <svg
               className="w-6 h-6"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
             </svg>
             <svg
               className="w-6 h-6"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
             </svg>
             <svg
               className="w-6 h-6"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
             </svg>
             <svg
               className="w-5 h-6"
@@ -70,7 +74,8 @@ const ProductWidget1 = (props: ProductWidget1Props) => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
-                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+              />
             </svg>
             <svg
               className="w-5 h-6"
@@ -82,7 +87,8 @@ const ProductWidget1 = (props: ProductWidget1Props) => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
-                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+              />
             </svg>
           </div>
           {/* Product Rating Starts */}
@@ -91,28 +97,25 @@ const ProductWidget1 = (props: ProductWidget1Props) => {
           <p className={"py-2 font-semibold pb-10"}>{product.warning}</p>
           <p className={"font-semibold text-2xl"}>
             <div className="inline-block relative px-2 text-left">
-              <div>
-                <button
-                  type="button"
-                  className="flex gap-2 justify-center items-center py-2 px-6 w-full text-lg font-medium text-gray-700 rounded-md border border-gray-500 shadow-sm dark:bg-gray-800 hover:bg-gray-50 focus:outline-none dark:text-gray-10s0 dark:hover:bg-gray-500"
-                  id="options-menu">
-                  Select option
-                  <svg
-                    width="20"
-                    height="20"
-                    fill="currentColor"
-                    viewBox="0 0 1792 1792"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      d="M1408 704q0 26-19 45l-448 448q-19 19-45 19t-45-19l-448-448q-19-19-19-45t19-45 45-19h896q26 0 45 19t19 45z" />
-                  </svg>
-                </button>
+              <div
+                className="flex gap-2 justify-center items-center py-2 px-6 w-full text-lg font-medium text-gray-700 rounded-md border border-gray-500 shadow-sm dark:bg-gray-800 hover:bg-gray-50 focus:outline-none dark:text-gray-10s0 dark:hover:bg-gray-500"
+                id="options-menu">
+                <label for="variations"> Select option</label>
+
+                <select name="Variations" id="size">
+                  <option value="90 tablets">{product.price}</option>
+                  <option value="20 capsules">{product.name}</option>
+                  <option value="10 capsules">10 capsules</option>
+                  <option value="5 capsules">5 capsules</option>
+                </select>
               </div>
             </div>
 
             {prettyPrice(product.price)}
           </p>
+
           <div className="inline-block relative py-10 px-2 text-left">
+            <App />
             <div>
               <div className="flex items-center">
                 <button
