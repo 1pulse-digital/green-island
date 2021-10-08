2fdbf784ca6e8e87095cb20091374732dad82164
Erick van der Linde <erick@andile.io>
2021-10-08 14:45:56 +0200

    Fix singleProductWidget.tsx

--- components/singleProductWidget.tsx
+++ components/singleProductWidget.tsx
@@ -1,11 +1,6 @@
 import { Product } from "../types/product";
 import Image from "next/image";
-import { strapiLoader } from "../lib/media";
 import { prettyPrice } from "../lib/calc";
-import ReactStars from "react-rating-stars-component";
-import React, { useState } from "react";
-import { StarIcon } from "@heroicons/react/solid";
-import App from "./app";
 
 export interface ProductWidget1Props {
   product: Product;
@@ -19,103 +14,83 @@ const ProductWidget1 = (props: ProductWidget1Props) => {
   }
 
   return (
-    <div className={"h-full w-full"}>
-      <div
-        className={"grid grid-cols-1 md:grid-cols-2 font-karla bg-gray-50  "}>
-        <div className=" relative self-center h-[600px] w-[600px]">
+    <div className={"w-full font-karla text-primary"}>
+      <div className={"grid lg:grid-cols-2"}>
+        {/* Image (* <--> md) */}
+        <div className="md:hidden flex place-content-center">
           <Image
-            layout="fill"
-            objectFit="cover"
-            loader={strapiLoader}
+            objectFit="contain"
+            height={300}
+            width={300}
             src={
               product.image?.formats.small.url ||
+              product.image?.formats.medium.url ||
               "https://image.shutterstock.com/image-vector/ui-image-placeholder-wireframes-apps-260nw-1037719204.jpg"
             }
-            alt={product.image?.alternativeText || "Product image"}
+            alt={product.image?.alternativeText || product.name}
           />
         </div>
-        {/* product description */}
-        <div className="  md:h-[100px]sm:h-[1500px] px-10 py-10  ">
-          <span className="text-4xl font-semibold truncate">
+
+        {/* Image (md <--> xl) */}
+        <div className="hidden md:flex xl:hidden place-content-center">
+          <Image
+            objectFit="contain"
+            height={400}
+            width={400}
+            src={
+              product.image?.formats.small.url ||
+              product.image?.formats.medium.url ||
+              "https://image.shutterstock.com/image-vector/ui-image-placeholder-wireframes-apps-260nw-1037719204.jpg"
+            }
+            alt={product.image?.alternativeText || product.name}
+          />
+        </div>
+
+        {/* Image (xl <--> *) */}
+        <div className="hidden xl:flex place-content-center">
+          <Image
+            objectFit="contain"
+            height={600}
+            width={600}
+            src={
+              product.image?.formats.small.url ||
+              product.image?.formats.medium.url ||
+              "https://image.shutterstock.com/image-vector/ui-image-placeholder-wireframes-apps-260nw-1037719204.jpg"
+            }
+            alt={product.image?.alternativeText || product.name}
+          />
+        </div>
+
+        {/* Name + *Variation + Description + Directions + Warning */}
+        <div className="p-4 xl:p-10">
+          {/* Name */}
+          <h1 className="text-2xl lg:text-4xl font-semibold truncate">
             {product.name}
-          </span>
+          </h1>
+          {/* Variations */}
+          {(product.variation) && (
+            <span className={"ml-4 text-sm"}>10 Amusja</span>
+          )}
+
           {/* Product Rating Starts */}
-          <div className={"flex flex-auto pt-6"}>
-            <ReactStars activeColor="blue" size={30} isHalf={true} />
+          {/* TODO: Add product ratings */}
+          {/*<ReactStars activeColor="blue" size={30} isHalf={true} />*/}
 
-            <svg
-              className="w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg">
-              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-            </svg>
-            <svg
-              className="w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg">
-              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-            </svg>
-            <svg
-              className="w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg">
-              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-            </svg>
-            <svg
-              className="w-5 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-              />
-            </svg>
-            <svg
-              className="w-5 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-              />
-            </svg>
-          </div>
-          {/* Product Rating Starts */}
-          <p className={"py-10 "}>{product.description}</p>
-          <p className={"py-2 font-semibold "}>{product.directions}</p>
-          <p className={"py-2 font-semibold pb-10"}>{product.warning}</p>
-          <p className={"font-semibold text-2xl"}>
-            <div className="inline-block relative px-2 text-left">
-              <div
-                className="flex gap-2 justify-center items-center py-2 px-6 w-full text-lg font-medium text-gray-700 rounded-md border border-gray-500 shadow-sm dark:bg-gray-800 hover:bg-gray-50 focus:outline-none dark:text-gray-10s0 dark:hover:bg-gray-500"
-                id="options-menu">
-                <label for="variations"> Select option</label>
+          {/* Description */}
+          <p className={"mt-6 lg:mt-8 font-semibold "}>Description</p>
+          <h3 className={"my-2 "}>{product.description}</h3>
 
-                <select name="Variations" id="size">
-                  <option value="90 tablets">{product.price}</option>
-                  <option value="20 capsules">{product.name}</option>
-                  <option value="10 capsules">10 capsules</option>
-                  <option value="5 capsules">5 capsules</option>
-                </select>
-              </div>
-            </div>
+          {/* Directions */}
+          <p className={"mt-6 lg:mt-8 font-semibold "}>Directions</p>
+          <p className={"my-2"}>{product.directions}</p>
 
-            {prettyPrice(product.price)}
-          </p>
+          {/* Warning */}
+          <p className={"mt-6 lg:mt-8 font-semibold "}>Warning</p>
+          <p className={"my-2 pb-10"}>{product.warning}</p>
+
+          <h2 className={"font-semibold text-2xl"}>{prettyPrice(product.price)}</h2>
 
           <div className="inline-block relative py-10 px-2 text-left">
-            <App />
             <div>
               <div className="flex items-center">
                 <button
