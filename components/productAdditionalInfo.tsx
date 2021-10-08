67dd2016d51fa3dba05b03c343613455610098f6
Erick van der Linde <erick@andile.io>
2021-10-08 14:42:51 +0200

    Fix productAdditionalInfo - Render ingredients - Fix/update styling

--- components/productAdditionalInfo.tsx
+++ components/productAdditionalInfo.tsx
@@ -1,6 +1,7 @@
 import { Tab } from "@headlessui/react";
 import classnames from "classnames";
 import { Product } from "../types/product";
+import { Ingredients } from "./products/ingredients";
 
 export interface ProductAdditionalInfoProps {
   product: Product;
@@ -8,17 +9,17 @@ export interface ProductAdditionalInfoProps {
 
 export const ProductAdditionalInfo = (props: ProductAdditionalInfoProps) => {
   return (
-    <div className="py-16 px-10 ml-16 w-3/4 sm:px-0">
+    <div className="px-2 lg:px-8 rounded mx-auto my-16 2xl:px-0 2xl:w-3/4 ">
       <Tab.Group>
         <Tab.List className="flex p-1 space-x-1 rounded-xl bg-secondary">
           <Tab
             className={({ selected }) =>
               classnames(
                 "w-full py-2.5 text-lg leading-5 font-medium text-primary rounded-lg",
-                "focus:outline-none  ring-white ring-opacity-60",
+                "focus:outline-none ring-white ring-opacity-60",
                 selected
                   ? "bg-white shadow"
-                  : "text-white hover:bg-white/[0.12] hover:text-white"
+                  : "text-white hover:bg-white/[0.12] hover:text-white",
               )
             }>
             Ingredients
@@ -30,7 +31,7 @@ export const ProductAdditionalInfo = (props: ProductAdditionalInfoProps) => {
                 "focus:outline-none  ring-white ring-opacity-60",
                 selected
                   ? "bg-white shadow"
-                  : "text-white hover:bg-white/[0.12] hover:text-white"
+                  : "text-white hover:bg-white/[0.12] hover:text-white",
               )
             }>
             Additional Information
@@ -42,7 +43,7 @@ export const ProductAdditionalInfo = (props: ProductAdditionalInfoProps) => {
                 "focus:outline-none  ring-white ring-opacity-60",
                 selected
                   ? "bg-white shadow"
-                  : "text-white hover:bg-white/[0.12] hover:text-white"
+                  : "text-white hover:bg-white/[0.12] hover:text-white",
               )
             }>
             Benefits
@@ -54,40 +55,32 @@ export const ProductAdditionalInfo = (props: ProductAdditionalInfoProps) => {
                 "focus:outline-none  ring-white ring-opacity-60",
                 selected
                   ? "bg-white shadow"
-                  : "text-white hover:bg-white/[0.12] hover:text-white"
+                  : "text-white hover:bg-white/[0.12] hover:text-white",
               )
             }>
             Reviews
           </Tab>
         </Tab.List>
 
-        <Tab.Panels className="mt-2">
+        <Tab.Panels className={"h-96 overflow-y-auto p-2"}>
           <Tab.Panel
-            className={classnames(
-              "bg-white rounded-xl p-3",
-              "focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60"
-            )}>
-            <div>{props.product.ingredients}</div>
+            className={"rounded-xl p-3 focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60"}
+          >
+            <Ingredients ingredients={props.product.ingredients} />
           </Tab.Panel>
           <Tab.Panel
-            className={classnames(
-              "bg-white rounded-xl p-3",
-              "focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60"
-            )}>
+            className={"rounded-xl p-3 focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60"}
+          >
             <div>{props.product.additional_information}</div>
           </Tab.Panel>
           <Tab.Panel
-            className={classnames(
-              "bg-white rounded-xl p-3",
-              "focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60"
-            )}>
+            className={"rounded-xl p-3 focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60"}
+          >
             <div>{props.product.benefits}</div>
           </Tab.Panel>
           <Tab.Panel
-            className={classnames(
-              "bg-white rounded-xl p-3",
-              "focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60"
-            )}>
+            className={"rounded-xl p-3 focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60"}
+          >
             <div>{props.product.warning}</div>
           </Tab.Panel>
         </Tab.Panels>
