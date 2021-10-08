7f189061ca3c3af6da975f445e87ccb12be4c4b3
Erick van der Linde <erick@andile.io>
2021-10-08 11:34:42 +0200

    Fix the blog navigation and styling

--- components/blog_navbar.tsx
+++ components/blog_navbar.tsx
@@ -1,44 +1,49 @@
 import { ArticleCategory } from "../types/articleCategory";
 import cn from "classnames";
-import { productTypes } from "../types/productTypes";
+
 export interface BlogNavProps {
   categories?: ArticleCategory[];
   onSelect: (name: string) => void;
   selectedCategory: string;
 }
 
+const CategoryOption = ({ name, onSelect, selectedCategory }: { name: string } & BlogNavProps) => {
+  return (
+    <div
+      onClick={(event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        onSelect(name);
+      }}
+      className={cn(
+        "w-full sm:w-auto text-primary rounded ring-secondary ring-1",
+        { "hover:border-secondary ring-primary hover:bg-secondary/20": selectedCategory !== name },
+        { "bg-secondary text-white": selectedCategory === name },
+      )}>
+      <button className="w-full sm:w-44 h-16 lg:w-60 lg:h-20 font-bold">
+        {name}
+      </button>
+    </div>
+  );
+};
+
 const BlogNav = (props: BlogNavProps) => {
   return (
-    <div className={"grid grid-cols-1 md:grid-cols-5 mb-10"}>
-      <div
-        onClick={(event) => {
-          event.preventDefault();
-          event.stopPropagation();
-          props.onSelect("All Posts");
-        }}
-        className={" "}>
-        <a
-          href=" "
-          className="grid py-10 px-24 font-bold text-primary hover:bg-secondary">
-          All Posts
-        </a>
-      </div>
+    <div className={"flex flex-wrap gap-2 px-2 sm:gap-4 sm:px-4 justify-center"}>
+      <CategoryOption
+        selectedCategory={props.selectedCategory}
+        onSelect={props.onSelect}
+        name={"All Posts"}
+      />
+
       {props.categories?.map((c) => {
         return (
-          <div
-            onClick={(event) => {
-              event.preventDefault();
-              event.stopPropagation();
-              props.onSelect(c.name);
-            }}
+          <CategoryOption
             key={c.id}
-            className={cn("hover:bg-secondary hover:text-white", {
-              "bg-secondary": props.selectedCategory === c.name,
-            })}>
-            <a href=" " className="grid py-10 px-24 font-bold text-primary">
-              {c.name}
-            </a>
-          </div>
+            selectedCategory={props.selectedCategory}
+            onSelect={props.onSelect}
+            name={c.name}
+          />
         );
       })}
     </div>
