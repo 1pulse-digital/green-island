bda1b0c9d728c89239998b531217eeebd0049900
Erick van der Linde <erick@andile.io>
2021-10-27 22:03:58 +0200

    Change to lighter text for disabled inputs

--- components/input.tsx
+++ components/input.tsx
@@ -19,7 +19,7 @@ export const Input = (props: InputProps) => {
   const name = props.name || props.id;
   const type = props.type || "text";
   const placeHolder = props.placeHolder || props.label;
-  const inputClassName = tailshake("w-full h-10 placeholder-transparent text-gray-900 disabled:text-gray-600 rounded border-gray-300 focus:ring-0 focus:outline-none peer focus:border-secondary", props.inputClassName);
+  const inputClassName = tailshake("w-full h-10 placeholder-transparent text-gray-900 disabled:text-gray-400 rounded border-gray-300 focus:ring-0 focus:outline-none peer focus:border-secondary", props.inputClassName);
   const labelClassName = tailshake("absolute left-2 -top-5 text-sm text-gray-600 transition-all peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:text-gray-600 peer-focus:-top-5 peer-focus:text-sm", props.labelClassName);
 
   return (
