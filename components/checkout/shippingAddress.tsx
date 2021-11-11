56e13c0d13f46fb39094f76c2e2dcc05e3a5515c
Erick van der Linde <erick@andile.io>
2021-11-11 08:41:44 +0200

    Lots of updates and refactoring for shippingAddress

--- components/checkout/shippingAddress.tsx
+++ components/checkout/shippingAddress.tsx
@@ -16,184 +16,191 @@ export interface ShippingAddressProps {
   setValues: (values: ShippingAddress) => void;
 }
 
-export const ShippingAddress = (props: ShippingAddressProps) => {
-  const { values, setValues } = props;
-  const placeRef = useRef<HTMLInputElement>(null);
-  const handleChange = (name: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValues({ ...values, [name]: event.target.value });
-  };
+export const breakdownGeoResult = (geoResult: google.maps.GeocoderResult) => {
+  const addressBreakdown = {
+    postal_code: "",
+    country: "",
+    province: "",
+    city: "",
+    area: "",
+    suburb: "",
+    street: "",
+    street_number: "",
+    formatted_address: geoResult.formatted_address,
+  } as Address;
 
-  const handleAddressChange = (value: string) => {
-    const addressBreakdown = {
-      postal_code: "",
-      country: "",
-      province: "",
-      city: "",
-      area: "",
-      suburb: "",
-      street: "",
-      street_number: "",
-      formatted_address: value,
-    } as Address;
-    setValues({ ...values, ...addressBreakdown });
-  };
+  console.log({ geoResult });
 
-  const handleSelectAddress = async (address: string) => {
-    const geoResultList = await geocodeByAddress(address);
-    if (!geoResultList || geoResultList.length < 1) {
-      return;
+  for (const ac of geoResult.address_components) {
+    switch (convertAddressComponentType(ac.types)) {
+      case "country" :
+        addressBreakdown.country = ac.long_name;
+        break;
+      case "postalCode" :
+        addressBreakdown.postal_code = ac.long_name;
+        break;
+      case "province" :
+        addressBreakdown.province = ac.long_name;
+        break;
+      case "city" :
+        addressBreakdown.city = ac.long_name;
+        break;
+      case "area" :
+        addressBreakdown.area = ac.long_name;
+        break;
+      case "suburb" :
+        addressBreakdown.suburb = ac.long_name;
+        break;
+      case "street" :
+        addressBreakdown.street = ac.long_name;
+        break;
+      case "streetNumber" :
+        addressBreakdown.street_number = ac.long_name;
+        break;
     }
-
-    const geoResult = geoResultList[0];
-    const addressBreakdown = {
-      postal_code: "",
-      country: "",
-      province: "",
-      city: "",
-      area: "",
-      suburb: "",
-      street: "",
-      street_number: "",
-      formatted_address: geoResult.formatted_address,
-    } as Address;
-
-    console.log({ geoResult });
-
-    for (const ac of geoResult.address_components) {
-      switch (convertAddressComponentType(ac.types)) {
-        case "country" :
-          addressBreakdown.country = ac.long_name;
-          break;
-        case "postalCode" :
-          addressBreakdown.postal_code = ac.long_name;
-          break;
-        case "province" :
-          addressBreakdown.province = ac.long_name;
-          break;
-        case "city" :
-          addressBreakdown.city = ac.long_name;
-          break;
-        case "area" :
-          addressBreakdown.area = ac.long_name;
-          break;
-        case "suburb" :
-          addressBreakdown.suburb = ac.long_name;
-          break;
-        case "street" :
-          addressBreakdown.street = ac.long_name;
-          break;
-        case "streetNumber" :
-          addressBreakdown.street_number = ac.long_name;
-          break;
-      }
-    }
-
-    setValues({
-      ...values, ...addressBreakdown,
-    });
-  };
-
-  return (
-    <div className={"bg-white p-10 "}>
-      <p className={"text-2xl font-semibold "}>Shipping Address</p>
-      <div className={"grid gap-8 mt-6"}>
-        <Input
-          id={"first-name"}
-          label={"First name"}
-          value={values.first_name}
-          onChange={handleChange("first_name")}
-        />
-
-        <Input
-          id={"last-name"}
-          label={"Last name"}
-          onChange={handleChange("last_name")}
-          value={values.last_name}
-        />
-
-        <Input
-          id={"company-name"}
-          label={"Company name (Optional)"}
-          onChange={handleChange("company_name")}
-          value={values.company_name}
-        />
-
-        <Input
-          id={"phoneNumber"}
-          label={"Phone number"}
-          onChange={handleChange("phone_number")}
-          value={values.phone_number}
-        />
-
-        <div className={cn("duration-1000", { "hidden": values.postal_code })}>
-          <PlacesAutocomplete
-            value={values.formatted_address}
-            onChange={handleAddressChange}
-            onSelect={handleSelectAddress}
-            searchOptions={{
-              componentRestrictions: {
-                country: "ZA",
-              },
-            }}
-          >
-            {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-              <div className={"relative"}>
-                <input
-                  {...getInputProps({
-                    placeholder: "Address",
-                    className: "w-full h-10 placeholder-transparent text-gray-900 rounded border-gray-300 focus:ring-0 focus:outline-none peer focus:border-secondary",
-                    disabled: false,
-                    id: "google_maps_address",
-                    ref: placeRef,
-                  })}
-                />
-                <label
-                  htmlFor={"google_maps_address"}
-                  className="absolute left-2 -top-5 text-sm text-gray-600 transition-all peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:text-gray-600 peer-focus:-top-5 peer-focus:text-sm"
-                >
-                  Address
-                </label>
-                <div className="grid gap-1 shadow">
-                  {loading && <div>Loading...</div>}
-                  {suggestions.map((suggestion, idx) => {
-                    const className = cn("p-2 hover:ring-2 ring-primary ring-inset cursor-pointer", { "bg-gray-100": suggestion.active });
-                    return (
-                      <div
-                        {...getSuggestionItemProps(suggestion, {
-                          className,
-                        })}
-
-                        key={idx}
-                      >
-                        <span>{suggestion.description}</span>
-                      </div>
-                    );
-                  })}
-                </div>
-              </div>
-            )}
-          </PlacesAutocomplete>
-        </div>
-
-        <div className={"grid w-full "}>
-          <h2 className={"text-lg text-gray-800 text-primary w-min"}>{values.country ? "Address" : ""}</h2>
-          <p className={"text-md text-gray-600 cursor-pointer"} onClick={() => {
-            handleAddressChange("");
-            placeRef.current?.focus();
-          }}>
-            <span className={""}>{values.street_number ? `${values.street_number} ${values.street}` : ""}</span><br />
-            <span className={""}>{values.suburb} </span><br />
-            {renderCity(values.city)}<br />
-            <span className={""}>{values.country}</span><br />
-            <span className={""}>{values.postal_code}</span>
-          </p>
-        </div>
-      </div>
-
-
-    </div>
-  );
+  }
+  return addressBreakdown;
 };
+
+export const ShippingAddress = (props: ShippingAddressProps) => {
+    const { values, setValues } = props;
+    const placeRef = useRef<HTMLInputElement>(null);
+
+    const handleChange = (name: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      setValues({ ...values, [name]: event.target.value });
+    };
+
+    const handleAddressChange = (value: string) => {
+      const addressBreakdown = {
+        postal_code: "",
+        country: "",
+        province: "",
+        city: "",
+        area: "",
+        suburb: "",
+        street: "",
+        street_number: "",
+        formatted_address: value,
+      } as Address;
+      setValues({ ...values, ...addressBreakdown });
+    };
+
+    const handleSelectAddress = async (address: string) => {
+      const geoResultList = await geocodeByAddress(address);
+      if (!geoResultList || geoResultList.length < 1) {
+        return;
+      }
+
+      const geoResult = geoResultList[0];
+      const addressBreakdown = breakdownGeoResult(geoResult);
+
+      setValues({
+        ...values, ...addressBreakdown,
+      });
+    };
+
+    return (
+      <div className={"bg-white p-10 "}>
+        <p className={"text-2xl font-semibold "}>Shipping Address</p>
+        <div className={"grid gap-8 mt-6"}>
+          <Input
+            id={"first-name"}
+            label={"First name"}
+            value={values.first_name}
+            onChange={handleChange("first_name")}
+          />
+
+          <Input
+            id={"last-name"}
+            label={"Last name"}
+            onChange={handleChange("last_name")}
+            value={values.last_name}
+          />
+
+          <Input
+            id={"company-name"}
+            label={"Company name (Optional)"}
+            onChange={handleChange("company_name")}
+            value={values.company_name}
+          />
+
+          <Input
+            id={"phoneNumber"}
+            label={"Phone number"}
+            onChange={handleChange("phone_number")}
+            value={values.phone_number}
+          />
+
+          <div className={cn("duration-1000", { "hidden": values.postal_code })}>
+            <PlacesAutocomplete
+              value={values.formatted_address}
+              onChange={handleAddressChange}
+              onSelect={handleSelectAddress}
+              searchOptions={{
+                componentRestrictions: {
+                  country: "ZA",
+                },
+              }}
+            >
+              {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                <div className={"relative"}>
+                  <input
+                    {...getInputProps({
+                      placeholder: "Address",
+                      className: "w-full h-10 placeholder-transparent text-gray-900 rounded border-gray-300 focus:ring-0 focus:outline-none peer focus:border-secondary",
+                      disabled: false,
+                      id: "google_maps_address",
+                      ref: placeRef,
+                    })}
+                  />
+                  <label
+                    htmlFor={"google_maps_address"}
+                    className="absolute left-2 -top-5 text-sm text-gray-600 transition-all peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:text-gray-600 peer-focus:-top-5 peer-focus:text-sm"
+                  >
+                    Address
+                  </label>
+                  <div className="grid gap-1 shadow">
+                    {loading && <div>Loading...</div>}
+                    {suggestions.map((suggestion, idx) => {
+                      const className = cn("p-2 hover:ring-2 ring-primary ring-inset cursor-pointer", { "bg-gray-100": suggestion.active });
+                      return (
+                        <div
+                          {...getSuggestionItemProps(suggestion, {
+                            className,
+                          })}
+
+                          key={idx}
+                        >
+                          <span>{suggestion.description}</span>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              )}
+            </PlacesAutocomplete>
+          </div>
+
+          <div className={"grid w-full "}>
+            <h2 className={"text-lg text-gray-800 text-primary w-min"}>{values.country ? "Address" : ""}</h2>
+            <p className={"text-md text-gray-600 cursor-pointer"} onClick={() => {
+              handleAddressChange("");
+              placeRef.current?.focus();
+            }}>
+              <span className={""}>{values.street_number ? `${values.street_number} ${values.street}` : ""}</span><br />
+              <span className={""}>{values.suburb} </span><br />
+              {renderCity(values.city)}<br />
+              <span className={""}>{values.country}</span><br />
+              <span className={""}>{values.postal_code}</span>
+            </p>
+          </div>
+        </div>
+
+
+      </div>
+    );
+  }
+;
 
 type addressComponentType =
   "postalCode"
