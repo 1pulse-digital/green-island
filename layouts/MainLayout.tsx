434ae4294c68061345b61aa5191fc57591058036
Erick van der Linde <erick@andile.io>
2022-03-13 21:28:41 +0200

    Add a redirect link to login page on authentication required page

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -5,6 +5,7 @@ import { SecondFooter } from "../components/secondFooter";
 import Head from "next/head";
 import { useAuthContext } from "../contexts/authContext";
 import { RoleName } from "../types/user";
+import Link from "next/link";
 
 export interface MainLayoutProps {
   children: ReactNode;
@@ -35,10 +36,15 @@ const MainLayout = (props: MainLayoutProps) => {
 
 
       {!accessAllowed &&
-      <div className={"py-20 text-center"}>You need to be logged in to view this page</div>
+        <>
+          <div className={"py-20 text-center"}>You need to be logged in to view this page</div>
+          <Link href="/login">
+            <a className={"py-20 text-center hover:text-secondary hover:underline"}>Go to login page</a>
+          </Link>
+        </>
       }
       {accessAllowed &&
-      <div className={"flex flex-col flex-grow"}>{props.children}</div>
+        <div className={"flex flex-col flex-grow"}>{props.children}</div>
       }
 
       <Footer />
