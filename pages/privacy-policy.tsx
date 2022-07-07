53beb7db0ab8380b538d283e66b1f42b8fca2303
Erick van der Linde <erick@andile.io>
2022-07-07 18:01:21 +0200

    Update privacy-policy content

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -4,8 +4,8 @@ import MainLayout from "../layouts/MainLayout";
 const PrivacyPolicy = () => {
   return (
     <MainLayout>
-      <div className={"bg-gray-100 pb-8"}>
-        <div className={"px-8 sm:px-12 lg:px-24 text-lg"}>
+      <div className={"bg-gray-100 pb-8 font-karla"}>
+        <div className={"px-8 sm:px-12 lg:px-24 text-lg grid gap-1"}>
           <h1 className={"font-semibold py-4"}>
             PAYMENT POLICY, TERMS & CONDITIONS OF VISITING THE PRACTICE AND
             RECEIVING HEALTH CARE AND RELATED SERVICES AT OR FROM THE PRACTICE
@@ -32,209 +32,197 @@ const PrivacyPolicy = () => {
             1. ROBIN’S PERFECT HEALTH IS A CASH PRACTICE:
           </h2>
           <p>
-            1.1. Robin’s Perfect Health is not contracted to any medical aid
-            and operates exclusively as a cash practice. Patients may submit
-            invoices for reimbursement by their medical aid, but no warranty
-            is made by the Robin’s Perfect Health for medical aid
-            reimbursement.
+            1.1. Robin’s Perfect Health is not contracted to any medical aid and operates exclusively as a cash
+            practice. Patients may submit invoices for reimbursement by their medical aid, but no warranty is made by
+            the Robin’s Perfect Health for medical aid reimbursement.
           </p>
           <p>
-            1.2. Medical aid coverage is an agreement
-            between the patient and the provider and is entirely independent
+            1.2. Medical aid coverage is an agreement between the patient and the provider and is entirely independent
             of Robin’s Perfect Health and its policies.
           </p>
           <p>
-            1.3. We bill consultations at a rate in excess of NHRPL tariffs, so medical
-            aids may not cover the costs of consultation in full.
+            1.3. We bill consultations at a rate in excess of NHRPL tariffs, so medical aids may not cover the costs of
+            consultation in full.
           </p>
           <p>
-            1.4. While the practice may assist in submitting the invoice to
-            Medical Aids on behalf of the patient, it remains the patient’s
-            sole responsibility to follow up with their medical aid regarding
-            reimbursement.
+            1.4. While the practice may assist in submitting the invoice to Medical Aids on behalf of the patient, it
+            remains the patient’s sole responsibility to follow up with their medical aid regarding reimbursement.
           </p>
           <p>
-            1.5. All patients must complete our Patient Information form before
-            seeing the practitioner. It is your responsibility to ensure that we
-            have your correct information. Robin’s Perfect Health takes no
-            responsibility for non-payment by medical aid schemes due to the
-            submission of incorrect or incomplete information.
+            1.5. All patients must complete our Patient Information form before seeing the practitioner. It is your
+            responsibility to ensure that we have your correct information. Robin’s Perfect Health takes no
+            responsibility for non-payment by medical aid schemes due to the submission of incorrect or incomplete
+            information.
           </p>
           <p>
-            1.6. Under no circumstances will Robin’s Perfect Health backdate
-            invoices for the purposes of claiming from medical aid.
+            1.6. Under no circumstances will Robin’s Perfect Health backdate invoices for the purposes of claiming from
+            medical aid.
           </p>
           <p>
-            1.7. We will submit your claims and assist you in any way we can to
-            help get your claims paid. Your medical scheme administrator may request
-            information directly from you. Your failure to comply in good time
-            with your medical scheme’s request may result in your claim’s denial.
+            1.7. We will submit your claims and assist you in any way we can to help get your claims paid. Your medical
+            scheme administrator may request information directly from you. Your failure to comply in good time with
+            your medical scheme’s request may result in your claim’s denial.
           </p>
 
           <h2 className={"font-semibold py-4"}>
             2. UPDATED CHANGE OF INFORMATION:
           </h2>
           <p>
-            2.1. It is the patient’s responsibility to update your information
-            whenever you have a change in address, employment, medical scheme
-            plan, option, benefits, etc. Failure to update information will
-            not exempt the patient from responsibility for all outstanding
-            accounts held with Robin’s Perfect Health.
+            2.1. It is the patient’s responsibility to update your information whenever you have a change in address,
+            employment, medical scheme plan, option, benefits, etc. Failure to update information will not exempt the
+            patient from responsibility for all outstanding accounts held with Robin’s Perfect Health.
           </p>
+
           <h2 className={"font-semibold py-4"}>
             3. PAYMENT:
           </h2>
-          <p>
-            3.1. Payment is expected in full at the time of your visit. We will
-            accept cash or credit card. EFTs are only accepted for product
-            orders for delivery or by prior arrangement with the Robin’s Perfect
-            Health management. Robin’s Perfect Health reserves the right to deny
-            the option to pay by EFT.
-          </p>
-          <p>
-            3.2. The consultation fee DOES NOT include additional finger-prick
-            blood tests, urine dipstick tests, or injectables. These tests are
-            optional, and the patient is liable for all additional tests
-            accepted.
-          </p>
-          <p>
-            3.3. Different consultation fees apply to Initial Consultations,
-            Follow Ups, and Specialised Diagnostic Test consultations, such as
-            Organic Acid Analysis. Ensure that you are familiar with the cost of
-            your consultation before you arrive.
-          </p>
-          <p>
-            3.4. A receipt detailing the services and products rendered on the
-            date of service will be issued to the patient immediately after
-            their consultation.
-          </p>
+
+          <h3>3.1. Consultations:</h3>
+          <div className={"pl-2 grid gap-1"}>
+            <p>
+              3.1.1. Payment is expected in full at the time of your visit. We will accept cash or credit card. EFTs are
+              only accepted for product orders for delivery or by prior arrangement with the Robin’s Perfect Health
+              management. Robin’s Perfect Health reserves the right to deny the option to pay by EFT.
+            </p>
+            <p>
+              3.1.2. The consultation fee DOES NOT include additional finger-prick blood tests, urine dipstick tests, or
+              injectables. These tests are optional, and the patient is liable for all additional tests accepted.
+            </p>
+            <p>
+              3.1.3. Different consultation fees apply to Initial Consultations, Follow Ups, and Specialised Diagnostic
+              Test consultations, such as Organic Acid Analysis. Ensure that you are familiar with the cost of your
+              consultation before you arrive.
+            </p>
+            <p>
+              3.1.4. A receipt detailing the services and products rendered on the date of service will be issued to the
+              patient immediately after their consultation.
+            </p>
+          </div>
+
+          <h3>3.2. Products:</h3>
+          <div className={"pl-2"}>
+            <p>
+              3.2.1. Products will not be released until payment in full is received.
+            </p>
+            <p>
+              3.2.2. Product orders may require a lead time of up to 7 working days. Orders that cannot be processed in
+              that time will be refunded in full.
+            </p>
+            <p>
+              3.2.3. Products ordered online without Dr Kohler’s recommendation following consultation will not be
+              released. Payments will be refunded with a 20% processing fee.
+            </p>
+          </div>
+
+
           <h2 className={"font-semibold py-4"}>
             4. NO SHOW POLICY:
           </h2>
           <p>
-            4.1. A cancellation fee amounting to the full consultation price is
-            payable if an appointment is not cancelled at least 12 hours prior
-            to the consultation.
+            4.1. A cancellation fee amounting to the full consultation price is payable if an appointment is not
+            cancelled at least 12 hours prior to the consultation.
           </p>
           <p>
-            4.2. If you miss 3 or more visits without
-            canceling or rescheduling 12 hours in advance you may be blocked
+            4.2. If you miss 3 or more visits without canceling or rescheduling 12 hours in advance you may be blocked
             from our practice.
           </p>
           <p>
-            4.3. Consideration of this cancellation fee will be given where
-            patients have endured exceptional circumstances relating to their
-            missed appointment entirely at the discretion of Robin’s Perfect
-            Health Management.
+            4.3. Consideration of this cancellation fee will be given where patients have endured exceptional
+            circumstances relating to their missed appointment entirely at the discretion of Robin’s Perfect Health
+            Management.
           </p>
+
 
           <h2 className={"font-semibold py-4"}>
             5. DIVORCED PARENTS OF PATIENTS:
           </h2>
           <p>
-            5.1. Enlisting at Robin’s Perfect Health practice, the adult who
-            signs a minor child into our practice on the day of service accepts
-            responsibility for payment.
+            5.1. Enlisting at Robin’s Perfect Health practice, the adult who signs a minor child into our practice on
+            the day of service accepts responsibility for payment.
           </p>
           <p>
-            5.2. This office does not promise to send bills or records to the
-            other parent/guardian for issues of payment or communication. We
-            will communicate about treatment and payment with the parent who
-            signs in that day.
+            5.2. This office does not promise to send bills or records to the other parent/guardian for issues of
+            payment or communication. We will communicate about treatment and payment with the parent who signs in that
+            day.
           </p>
           <p>
-            5.3. Parents must communicate with each other about the treatment
-            and payment issues pertaining to their minor children, or supply as
-            with a copy of a Settlement Agreement laying out the terms of
-            payment for the minor’s medical expenses, and which was made an
-            order of the court. Perfect Health will not mediate between parents
-            or other parties for payment of bills.
+            5.3. Parents must communicate with each other about the treatment and payment issues pertaining to their
+            minor children, or supply as with a copy of a Settlement Agreement laying out the terms of payment for the
+            minor’s medical expenses, and which was made an order of the court. Perfect Health will not mediate between
+            parents or other parties for payment of bills.
           </p>
-
 
           <h2 className={"font-semibold py-4"}>
             6. NON-PAYMENT:
           </h2>
-          <p>6.1. If your account is over 60 days past due, you will receive a
-            statement indicating that you have 30 days to pay your account in
-            full. Partial payments will not be accepted unless you have
-            contacted our office and negotiated a payment arrangement. Please be
-            aware that if a balance remains unpaid, we will turn your account
-            over to a collection agency after the 90th day past due.
+          <p>
+            6.1. If your account is over 60 days past due, you will receive a statement indicating that you have 30 days
+            to pay your account in full. Partial payments will not be accepted unless you have contacted our office and
+            negotiated a payment arrangement. Please be aware that if a balance remains unpaid, we will turn your
+            account over to a collection agency after the 90th day past due.
           </p>
 
           <h2 className={"font-semibold py-4"}>
             7. WAITING TIMES:
-
           </h2>
           <p>
-            7.1. Please note that due to complicated cases or
-            emergencies we may run late in attending to your scheduled
-            appointment time. This is an unavoidable feature of medical rooms
-            where serious and difficult medical cases are handled. We will do
-            our best to let you know (at the telephone numbers you have provided
-            to us) when we are running behind schedule. Having an appointment
-            begin late does not exempt you from payment in full for services and
-            products rendered during your consultation.
+            7.1. Please note that due to complicated cases or emergencies we may run late in attending to your scheduled
+            appointment time. This is an unavoidable feature of medical rooms where serious and difficult medical cases
+            are handled. We will do our best to let you know (at the telephone numbers you have provided to us) when we
+            are running behind schedule. Having an appointment begin late does not exempt you from payment in full for
+            services and products rendered during your consultation.
           </p>
           <p>
-            7.2. Visits that begin late due to a patient’s late arrival will be
-            charged the full visit fee, regardless of the amount of time spent
-            in consult. Consultations cannot be extended to make up for time
-            lost due to late arrival.
+            7.2. Visits that begin late due to a patient’s late arrival will be charged the full visit fee, regardless
+            of the amount of time spent in consult. Consultations cannot be extended to make up for time lost due to
+            late arrival.
           </p>
 
           <h2 className={"font-semibold py-4"}>
             8. RETURNS POLICY:
           </h2>
+          <p>
+            8.1. Returned products will only be refunded if they are returned in perfect condition with all seals intact
+            within 14 days of sale.
+          </p>
+          <p>
+            8.2. Refunds for returned products will be in the form of in-store credit.
+          </p>
+          <p>
+            8.3. You are responsible for checking the ingredients of your medication for known allergies. Reactions to
+            medications will not qualify you for a refund unless the product is found to be defective.
+          </p>
 
-          <p>
-            8.1. Returned products will only be refunded if they are returned in
-            perfect condition with all seals intact within 14 days of sale.{" "}
-          </p>
-          <p>
-            8.2. You are responsible for checking the ingredients of your
-            medication for known allergies. Reactions to medications will not
-            qualify you for a refund unless the product is found to be
-            defective.
-          </p>
           <h2 className={"font-semibold py-4"}>
             9. CONFIDENTIALITY:
           </h2>
+
           <p>
-            9.1. We will keep all of patient information confidential, including
-            information for children 12 years and older. Information pertaining
-            to children younger than 12 will be shared with the legal guardian
-            that brought the child to the consultation.
+            9.1. We will keep all of patient information confidential, including information for children 12 years and
+            older. Information pertaining to children younger than 12 will be shared with the legal guardian that
+            brought the child to the consultation.
           </p>
           <p>
-            9.2. The law prohibits communication with any individual, other than
-            those nominated by the patient or legal guardian in writing.
-            Patient’s information will only be released to other practitioners,
-            individuals or insurance companies if an Authorisation for Release
-            of Personal Information form has been completed.
+            9.2. The law prohibits communication with any individual, other than those nominated by the patient or legal
+            guardian in writing. Patient’s information will only be released to other practitioners, individuals or
+            insurance companies if an Authorisation for Release of Personal Information form has been completed.
           </p>
           <p>
-            9.3. To motivate specific treatments to your medical scheme we may
-            have to provide them with information on your health history and
-            other health information. To submit an account to a medical scheme
-            we must provide, in terms of the law, codes on the account that
-            discloses information to the scheme about your health condition.
-            Your signature on the Patient Information Form you complete prior to
-            your first consultation indicates that you agree to this. If you do
-            not agree, please inform the practice without delay.
+            9.3. To motivate specific treatments to your medical scheme we may have to provide them with information on
+            your health history and other health information. To submit an account to a medical scheme we must provide,
+            in terms of the law, codes on the account that discloses information to the scheme about your health
+            condition. Your signature on the Patient Information Form you complete prior to your first consultation
+            indicates that you agree to this. If you do not agree, please inform the practice without delay.
           </p>
 
           <h2 className={"font-semibold py-4"}>
             10. PATIENT DUTIES:
           </h2>
           <p>
-            10.1. In terms of the National Health Act you or your family or
-            other persons that comes to the practice with you, should not harass
-            the doctor, nurses or other staff members, and should treat them
-            with respect. Harassment includes rumours, swearing, verbal abuse,
-            arguments, property damage, vandalism, physical assaults,
-            psychological trauma, anger-related incidents.
+            10.1. In terms of the National Health Act you or your family or other persons that comes to the practice
+            with you, should not harass the doctor, nurses or other staff members, and should treat them with respect.
+            Harassment includes rumours, swearing, verbal abuse, arguments, property damage, vandalism, physical
+            assaults, psychological trauma, anger-related incidents.
           </p>
           <p>
             10.2. This practice follows a zero-tolerance policy against harassment.
